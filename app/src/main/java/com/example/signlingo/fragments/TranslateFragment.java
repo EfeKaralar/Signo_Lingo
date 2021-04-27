5b1062010697af14198fc00abccb48b48e915e52
EfeKaralar <alpefeka2001@gmail.com>
2021-04-27 21:45:40 +0300

    Commit

--- app/src/main/java/com/example/signlingo/fragments/TranslateFragment.java
+++ app/src/main/java/com/example/signlingo/fragments/TranslateFragment.java
@@ -2,59 +2,36 @@ package com.example.signlingo.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.signlingo.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TranslateFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.sql.Time;
+
 public class TranslateFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public static final String TAG = "TranslateFragment";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    EditText etTranslator;
+    Button btnTranslate;
+    ImageView ivTranslation;
 
     public TranslateFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TranslateFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TranslateFragment newInstance(String param1, String param2) {
-        TranslateFragment fragment = new TranslateFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +39,154 @@ public class TranslateFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_translate, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        etTranslator = view.findViewById(R.id.etTranslator);
+        btnTranslate = view.findViewById(R.id.btnTranslate);
+        ivTranslation = view.findViewById(R.id.ivTranslation);
+
+        btnTranslate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    translate();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void translate() throws InterruptedException {
+        if (etTranslator.getText() == null){
+            Toast.makeText(getContext(), "Please enter something to the translator", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String text = etTranslator.getText().toString().toLowerCase();
+        int i = 0;
+        while (i<text.length()){
+            int j = i;
+            textToImage(text, j);
+            i++;
+
+        }
+    }
+    public void textToImage(String text, int i){
+
+        if (text.substring(i).equals(" ")){
+            ivTranslation.setImageResource(R.drawable.ic_baseline_space_bar_24);
+        }
+        else {
+            String letter = text.substring(i).toLowerCase();
+            Log.d(TAG, "textToImage letter: "+ letter);
+            switch (letter) {
+                case "a":
+                    ivTranslation.setImageResource(R.drawable.a);
+                    break;
+                case "b":
+                    ivTranslation.setImageResource(R.drawable.b);
+                    break;
+                case "c":
+                    ivTranslation.setImageResource(R.drawable.c);
+                    break;
+
+                case "d":
+                    ivTranslation.setImageResource(R.drawable.d);
+                    break;
+
+                case "e":
+                    ivTranslation.setImageResource(R.drawable.e);
+                    break;
+
+                case "f":
+                    ivTranslation.setImageResource(R.drawable.f);
+                    break;
+
+                case "g":
+                    ivTranslation.setImageResource(R.drawable.g);
+                    break;
+
+                case "h":
+                    ivTranslation.setImageResource(R.drawable.h);
+                    break;
+
+                case "i":
+                    ivTranslation.setImageResource(R.drawable.i);
+                    break;
+
+                case "j":
+                    ivTranslation.setImageResource(R.drawable.j);
+                    break;
+
+                case "k":
+                    ivTranslation.setImageResource(R.drawable.k);
+                    break;
+
+                case "l":
+                    ivTranslation.setImageResource(R.drawable.l);
+                    break;
+
+                case "m":
+                    ivTranslation.setImageResource(R.drawable.m);
+                    break;
+
+                case "n":
+                    ivTranslation.setImageResource(R.drawable.n);
+                    break;
+
+                case "o":
+                    ivTranslation.setImageResource(R.drawable.o);
+                    break;
+
+                case "p":
+                    ivTranslation.setImageResource(R.drawable.p);
+                    break;
+
+                case "q":
+                    ivTranslation.setImageResource(R.drawable.q);
+                    break;
+
+                case "r":
+                    ivTranslation.setImageResource(R.drawable.r);
+                    break;
+
+                case "s":
+                    ivTranslation.setImageResource(R.drawable.s);
+                    break;
+
+                case "t":
+                    ivTranslation.setImageResource(R.drawable.t);
+                    break;
+
+                case "u":
+                    ivTranslation.setImageResource(R.drawable.u);
+                    break;
+
+                case "v":
+                    ivTranslation.setImageResource(R.drawable.v);
+                    break;
+
+                case "w":
+                    ivTranslation.setImageResource(R.drawable.w);
+                    break;
+
+                case "x":
+                    ivTranslation.setImageResource(R.drawable.x);
+                    break;
+
+                case "y":
+                    ivTranslation.setImageResource(R.drawable.y);
+                    break;
+
+                case "z":
+                    ivTranslation.setImageResource(R.drawable.z);
+                    break;
+
+            }
+        }
     }
 }
